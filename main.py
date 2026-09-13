6459e78950ce242272e12042bafd7897ae8c6509
paulvxx <psv51037@gmail.com>
2026-09-13 10:47:25 -0400

    added stuff to main

--- main.py
+++ main.py
@@ -8,21 +8,31 @@ from utils import utils, fol_string_encoder
 
 fibonacci = False
 
+# add starting predicate condition
+cnf_format = [[(1,'a','a',True)]]
+
 if fibonacci:
     #h_iList, v_iList = powers_of_two_gen.gen_powers_of_two_cycle_model(5)
-    h_iList, v_iList = fibonacci_gen.gen_fibonacci_encoding_model(10)
+    _, h_iList, v_iList = fibonacci_gen.gen_fibonacci_encoding_model(10)
 
     print(f"Number of State Combinations present : {utils.get_state_count_adj_mappings(h_iList, v_iList)}")
 
-    # add starting predicate condition
-    cnf_format = [[(1,'a','a',True)]]
     cnf_format.extend(utils.implication_list_to_cnf_AEA(h_iList, v_iList, explicit_disjoint=False))
 
     formula_str = fol_string_encoder.to_fol_cnf_formula(cnf_format, add_exists_front=True, pretty=True)
     print(formula_str)
 else:
-    #pack = powers_of_two_gen.gen_powers_of_two_cycle_model(6)
+    _, h_iList, v_iList = powers_of_two_gen.gen_powers_of_two_cycle_model(7)
+    print(f"Number of State Combinations present : {utils.get_state_count_adj_mappings(h_iList, v_iList)}")
+    cnf_format.extend(utils.implication_list_to_cnf_AEA(h_iList, v_iList, explicit_disjoint=False))
+    formula_str = fol_string_encoder.to_fol_cnf_formula(cnf_format, add_exists_front=True, pretty=True)
+    print(formula_str)
     #print(pack[0])
     #print("")
-    pack = powers_of_two_lin_index_gen.gen_powers_of_two_cycle_linear_block_model(6)
-    print(pack[0])
+    _, h_iList, v_iList = powers_of_two_lin_index_gen.gen_powers_of_two_cycle_linear_block_model(7)
+    print(f"Number of State Combinations present : {utils.get_state_count_adj_mappings(h_iList, v_iList)}")
+    cnf_format.extend(utils.implication_list_to_cnf_AEA(h_iList, v_iList, explicit_disjoint=False))
+    formula_str = fol_string_encoder.to_fol_cnf_formula(cnf_format, add_exists_front=True, pretty=True)
+    print(formula_str)
+    #print(pack[0])
+    #print("")
